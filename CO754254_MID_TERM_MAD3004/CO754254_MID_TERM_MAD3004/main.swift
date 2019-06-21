137d0fe711971fa0de8ad2bd1bf5cdb1c0e5ac60
ashish pratheesh <ashishpradeesh@gmail.com>
2019-06-20 20:01:26 -0400

    created 3 objects of customers

--- CO754254_MID_TERM_MAD3004/CO754254_MID_TERM_MAD3004/main.swift
+++ CO754254_MID_TERM_MAD3004/CO754254_MID_TERM_MAD3004/main.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 
-// cstomer object
+// customer object
 let customer1 = customer()
 customer1.customerId = "1"
 customer1.firstname = "jack"
@@ -17,4 +17,32 @@ customer1.lastname = "cruise "
 customer1.emailAddress = " jackcruse@gail.com"
 customer1.totalAmounToPay = " $ 100"
 
+// customer object
+let customer2 = customer()
+customer2.customerId = "2"
+customer2.firstname = "jack"
+customer2.lastname = "cruise "
+customer2.emailAddress = " jackcruse@gail.com"
+customer2.totalAmounToPay = " $ 100"
 
+// customer object
+let customer3 = customer()
+customer3.customerId = "3"
+customer3.firstname = "jack"
+customer3.lastname = "cruise "
+customer3.emailAddress = " jackcruse@gail.com"
+customer3.totalAmounToPay = " $ 100"
+
+
+
+
+var choice : Int?
+   choice = 0
+print("enter customer Id")
+let customerId = readLine()
+
+switch choice!
+{
+case 1: print(customer1)
+
+}
